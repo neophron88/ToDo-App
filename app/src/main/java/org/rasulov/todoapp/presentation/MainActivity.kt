32903298dfe248eca610259fa03fb29b4bc83575
Fazliddin R <itrf0088@gmail.com>
2023-10-11 13:29:49 +0500

    minor 2

--- app/src/main/java/org/rasulov/todoapp/presentation/MainActivity.kt
+++ app/src/main/java/org/rasulov/todoapp/presentation/MainActivity.kt
@@ -20,10 +20,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-
         setupActionBarWithNavController(navController)
-
-
     }
 }
